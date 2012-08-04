ee9629cfd0b90e91e1bd24b47515d9b60afbd82d
Joona Somerkivi <joona.somerkivi@solinor.com>
2012-08-05 00:18:25 +0300

    Included base url definition

--- Public/index.php
+++ Public/index.php
@@ -4,7 +4,8 @@ include 'JSomerstone/Cimbic/cimbicAutoloader.php';
 define('SITE_PATH_PREFIX', 'somerkivi.net');
 
 $contentManager = new JSomerstone\Cimbic\ContentManager(
-        dirname(__DIR__)
+        dirname(__DIR__),
+        'somerkivi.net'
 );
 
 $contentManager->execute();
